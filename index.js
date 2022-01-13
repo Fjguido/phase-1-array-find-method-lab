6d1f7244acc064a262ebf05d63b9584cb609b379
Francis Guido <fjguido97@gmail.com>
2022-01-13 10:51:28 -0500

    commit tree 077d7a1691e4df779b5fc4933c637499fafa0984

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+// superbowlWin = (record) => {
+//     const result = record.find(record => record.result === "W");
+//     return !!result ? result.year : undefined;
+// }
+
+
+//different way
+function superbowlWin(teamYears) { 
+    console.log(teamYears)
+    const winningRecord = teamYears.find(yearDate => yearDate.result === "W")
+    return winningRecord ? winningRecord.year : undefined;
+    
+}
+console.log(winningRecord)
+//    
+
+//example - array.find(inside paranthesis includes the whole element) - for this example all the records
+//? is a ternary operator - expression to see if a result is true or false
+//since result is true it returned true if it was false it would return undefined
+//what comes before the arrow is a parameter
